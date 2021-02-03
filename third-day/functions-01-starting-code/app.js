ddf5326b641909fd90047a9f1f03622020c5de0f
git <abonini2000@gmail.com>
2021-02-02 21:38:09 -0300

    add players logic

--- third-day/functions-01-starting-code/app.js
+++ third-day/functions-01-starting-code/app.js
@@ -1,19 +1,43 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = PAPER;
 
-
-
-const strat = function gameStart(){
-    console.log("game is starting...")
+let gameISRunning = false;
+const getPlayerAction = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (
+    selection !== "rock" &&
+    selection !== "paper" &&
+    selection !== "scissors"
+  ) {
+    alert("invalid choice, we choice paper for you!");
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
 
+const strat = function gameStart() {};
 
 const person = {
-  greet:  function ola(){
-        console.log("Olá Amigo");
-    }
+  greet: function ola() {
+    console.log("Olá Amigo");
+  },
 };
 console.log(person.name);
 
 console.log(typeof gameStart);
-startGameBtn.addEventListener('click', gameStart);
+startGameBtn.addEventListener("click", function () {
+    if(gameISRunning){
+        return;
+    }
+    gameISRunning = true;
+  console.log("game is starting...");
+  const player = getPlayerAction();
+  console.log(player);
+});
