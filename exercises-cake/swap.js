5e561b95dacc7de866ae222bf8ad248c60afcbb8
git <abonini2000@gmail.com>
2021-02-16 09:29:36 -0300

    data structures basics

---
+++ exercises-cake/swap.js
@@ -0,0 +1,19 @@
+function reverse(arrayOfChars) {
+
+    let leftIndex = 0;
+    let rightIndex = arrayOfChars.length - 1;
+  
+    while (leftIndex < rightIndex) {
+  
+      // Swap characters
+      const temp = arrayOfChars[leftIndex];
+      arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+      arrayOfChars[rightIndex] = temp;
+  
+      // Move towards middle
+      leftIndex++;
+      rightIndex--;
+    }
+    console.log(arrayOfChars);
+  }
+  reverse(['h','e','l','l','o']);
