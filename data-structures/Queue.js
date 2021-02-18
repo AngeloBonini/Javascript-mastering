56698a274962ba497b7a333bb0177505330093e9
git <abonini2000@gmail.com>
2021-02-18 09:33:37 -0300

    queue and quicksort

---
+++ data-structures/Queue.js
@@ -0,0 +1,68 @@
+class QueueNode{
+constructor(value){
+    this._value = value;
+    this._next = null; 
+}
+set next(value){
+    this._next = value;
+}
+get next(){
+    return this._next;
+}
+get value(){
+    return this._value;
+}
+
+}
+
+class Queue{
+    cosntructor(value){
+        this._size=0;
+
+        if(value === undefined){
+            this._first = null;
+            this._last = null;
+        }
+        else{
+            this.enqueue(value);
+        }
+    }
+    get size(){
+        return this._size;
+    }
+    get empty(){
+        return this._size === 0;
+    }
+
+    enqueue(value){
+        let newNode = new QueueNode(value);
+        if(this.empty){
+            this._first = newNode;
+        }
+        else{
+            this._last.next = newNode;
+        }
+        this._last = newNode;
+        this._size++;
+    }
+    dequeue(){
+        let itemToRemove = this._first;
+        this._first = itemToRemove.next;
+        this._size--;
+        return itemToRemove.value;
+    }
+    peek(){
+        return this._first.value;
+    }
+    get headANdTail(){
+        return(this.dequeue(),this);
+    }
+
+    toArray(){
+        let array = [];
+        while(this._size > 0){
+            array.push(this.dequeue());
+        }
+        return array;
+    }
+}
