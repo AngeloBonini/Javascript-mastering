19444ce2260770ece1f9808feda25d7d06cbf1ff
git <abonini2000@gmail.com>
2021-03-27 23:32:49 -0300

    selecting button to control the modal

---
+++ DOM-day/dom-06-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,8 @@
+const addMovieModal = document.getElementById('add-modal');
+
+// const addMovieModal = document.body.children[1];
+
+console.log(addMovieModal);
+
+const startAddMovieButton = document.querySelector('header').lastElementChild;
+console.log(startAddMovieButton);
