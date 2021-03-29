536d86ea77ac79d13378a9e09e96b9371f551596
git <abonini2000@gmail.com>
2021-03-29 07:58:30 -0300

    interacting with the modal

--- DOM-day/dom-06-demo-project-setup/assets/scripts/app.js
+++ DOM-day/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,5 +1,4 @@
 const addMovieModal = document.getElementById('add-modal');
-
 // const addMovieModal = document.body.children[1];
 
 console.log(addMovieModal);
@@ -7,8 +6,26 @@ console.log(addMovieModal);
 const startAddMovieButton = document.querySelector('header').lastElementChild;
 console.log(startAddMovieButton);
 
+const backdrop = document.getElementById('backdrop')
+
+const cancelAddMovieButton = document.getElementById('modal_cancel');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible')
+}
 const toggleMovieModal = ()=>{
-    addMovieModal.classList.toggle('visible')
+    addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
+const cancelAddMovie = () =>{
+    toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
+console.log(cancelAddMovieButton)
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie );
