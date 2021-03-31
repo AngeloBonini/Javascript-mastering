5b93750e31d0775077c3e214fdb0cb57ea1c7774
git <abonini2000@gmail.com>
2021-03-31 08:58:51 -0300

    different types of arrays

--- Array-day/3.1 arrays-02-creating-arrays.zip/arrays-02-creating-arrays/app.js
+++ Array-day/3.1 arrays-02-creating-arrays.zip/arrays-02-creating-arrays/app.js
@@ -10,8 +10,23 @@ console.log(yetMoreNumbers);
 const moreNumbers = new Array(5);
 console.log(moreNumbers);
 
-const listItems = document.querySelectorAll('li');
+const listItems = document.querySelectorAll("li");
 console.log(listItems);
 
 const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
+
+const hobbies = ["cooking", "beatbox"];
+const personalData = [20, "Angelo", { moreDetails: [] }];
+
+const analyticsData = [
+  [1.4, 2.2],
+  [3.1, -5.7],
+  [2.4, 5.6],
+];
+
+for (const data of analyticsData) {
+  for (const datPoint of data) {
+      console.log(datPoint);
+  }
+}
