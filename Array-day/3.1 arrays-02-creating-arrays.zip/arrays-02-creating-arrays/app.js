dcd6520b47beeb36b444c540feba3d398933fa9c
git <abonini2000@gmail.com>
2021-03-31 08:29:32 -0300

    initializing arrays

--- Array-day/3.1 arrays-02-creating-arrays.zip/arrays-02-creating-arrays/app.js
+++ Array-day/3.1 arrays-02-creating-arrays.zip/arrays-02-creating-arrays/app.js
@@ -4,8 +4,11 @@ console.log(numbers);
 // const moreNumbers = Array(5, 2);
 // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+const yetMoreNumbers = Array.of(1, 2);
+console.log(yetMoreNumbers);
+
+const moreNumbers = new Array(5);
+console.log(moreNumbers);
 
 const listItems = document.querySelectorAll('li');
 console.log(listItems);
