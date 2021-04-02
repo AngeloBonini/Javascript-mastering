ff4c7b117b5e55d5fe88d7bf972e6c7f0aef843b
git <abonini2000@gmail.com>
2021-04-01 22:12:06 -0300

    reduce method

--- Array-day/3.1 arrays-02-creating-arrays.zip/arrays-02-creating-arrays/app.js
+++ Array-day/3.1 arrays-02-creating-arrays.zip/arrays-02-creating-arrays/app.js
@@ -31,8 +31,7 @@ for (const data of analyticsData) {
   }
 }
 
-
-const otherHobbies =  ["cooking", "beatbox", "sports"];
+const otherHobbies = ["cooking", "beatbox", "sports"];
 otherHobbies.unshift("coding");
 const popedItem = otherHobbies.pop();
 const shifted = otherHobbies.shift();
@@ -40,42 +39,41 @@ const shifted = otherHobbies.shift();
 // console.log(popedItem);
 // console.log(shifted);
 
-otherHobbies[1]="BEATBOX";
-otherHobbies[5]="Reading";
+otherHobbies[1] = "BEATBOX";
+otherHobbies[5] = "Reading";
 // console.log(otherHobbies, otherHobbies[3]);
 
 otherHobbies[1] = "coding";
 // console.log(otherHobbies, otherHobbies[4]);
 
-otherHobbies.splice(1, 0, "Good" );
+otherHobbies.splice(1, 0, "Good");
 
 // console.log(otherHobbies);
 
-
-const testsResults = [1, 7, 3, 7, 5.9, -10,8.5, 7 ];
+const testsResults = [1, 7, 3, 7, 5.9, -10, 8.5, 7];
 // const storedResults = testsResults.slice(-6, -1);
-const storedResults = testsResults.concat([2.99, 4])
-testsResults.push(6.53); 
+const storedResults = testsResults.concat([2.99, 4]);
+testsResults.push(6.53);
 
 // console.log(testsResults.includes(8.5));
 // console.log(testsResults, storedResults)
 //  console.log(testsResults.indexOf(7));
 //  console.log(testsResults.lastIndexOf(7));
-const personData = [{ name: "Gelo"}, {name: "Max"}];
+const personData = [{ name: "Gelo" }, { name: "Max" }];
 /**
  * indexOf doesnot work for obejcts as they are references to values.
  * this method creates a new array and cause of that, the reference is lost
  * @author: Angelo.
- */ 
+ */
 // console.log(personData.indexOf({name: "Max"}));
 
 const Max = personData.find((person, index, persons) => {
   return person.name === "Max";
-})
-Max.name ="Anna";
+});
+Max.name = "Anna";
 // console.log(Max, personData);
 
-const maxIndex = personData.findIndex((person, index, persons) =>{
+const maxIndex = personData.findIndex((person, index, persons) => {
   return person.name === "Gelo";
 });
 // console.log(maxIndex);
@@ -87,30 +85,28 @@ const prices = [9.99, 3.26, 2.1, 7.56, 4.86];
 const tax = 0.23;
 const taxAdjustedPrices = [];
 
-prices.forEach((price, idx, prices) =>{
-  const priceObj = { index: idx, taxAdjPrice:price * (1+tax) }
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
   taxAdjustedPrices.push(priceObj);
-})
+});
 console.log(prices);
 
- const taxAdjustedPricesMap = prices.map((price, idx, prices) =>{
-  const priceObj = { index: idx, taxAdjPrice:price * (1+tax) }
+const taxAdjustedPricesMap = prices.map((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
   return priceObj;
-})
+});
 
 console.log(taxAdjustedPricesMap);
 /**
  * reverse() and sort() methods
- * using functions inside sort to avoid string default sorting. 
+ * using functions inside sort to avoid string default sorting.
  */
-const sortedPrices = prices.sort((a,b)=>{
-  if(a > b){
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
     return 1;
-  }
-  else if (a === b){
+  } else if (a === b) {
     return 0;
-  }
-  else{
+  } else {
     return -1;
   }
 });
@@ -121,13 +117,19 @@ console.log(sortedPrices.reverse());
 /**
  * Original way with arrow functions.
  */
-const filteredArr = prices.filter((price, index, prices)=>{
+const filteredArr = prices.filter((price, index, prices) => {
   return price > 6;
-})
+});
 /**
  * Only arrow functions way.
  */
-const filteredArr2 = prices.filter(price => price > 6);
+const filteredArr2 = prices.filter((price) => price > 6);
 
+console.log(filteredArr2);
 
-console.log(filteredArr2 );
+/**
+ * Reduce method
+ */
+const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+console.log(sum);
