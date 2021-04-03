558781fdd6c7e3a197d60e442c4e3e71510a9520
git <abonini2000@gmail.com>
2021-04-03 18:42:17 -0300

    recap basics on objects

---
+++ objects_lessions/assets/scripts/objects.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'Gelo',
+    age: 20,
+    hobbies: ['batbox', 'cooking', 'reading'],
+    greet:
+    function(){
+        alert("Hello There!!");
+    }
+};
+person.greet();
