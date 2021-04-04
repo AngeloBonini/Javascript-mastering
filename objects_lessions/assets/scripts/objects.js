222d6faa46e4aa4ee87ee6172c806db9825030db
git <abonini2000@gmail.com>
2021-04-04 17:50:28 -0300

    adding the filter funcionallity with filter() method

--- objects_lessions/assets/scripts/objects.js
+++ objects_lessions/assets/scripts/objects.js
@@ -1,33 +1,38 @@
 const addMovieBtn = document.getElementById('add-movie-btn')
 const searchBtn = document.getElementById('search-btn')
 
-const movies = [];
+const movies = []
 
-const rendermovies = () => {
-const movieList = document.getElementById('movie-list');
+const rendermovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list')
 
+  if (movies.length === 0) {
+    movieList.classList.remove('visible')
+  } else {
+    movieList.classList.add('visible')
+  }
+  movieList.innerHTML = ''
 
-if(movies.length === 0 ){
-    movieList.classList.remove('visible');
-}else{
-    movieList.classList.add('visible');
-}
-movieList.innerHTML = '';
+  const fileteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
 
-movies.forEach((movie)=>{
-
-    const movieEl = document.createElement('li');
-    text = movie.info.title + ' - ';
-    for ( key in movie.info){
-        if(key !== 'title'){
-            text = text + `${key}: ${movie.info[key]}`;
-        }
+  fileteredMovies.forEach((movie) => {
+    const movieEl = document.createElement('li')
+    text = movie.info.title + ' - '
+    for (key in movie.info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${movie.info[key]}`
+      }
     }
-    movieEl.textContent = text;
-    movieList.append(movieEl);
-});
-
-};
+    movieEl.textContent = text
+    movieList.append(movieEl)
+  })
+}
+const searchMoviesHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value
+  rendermovies(filterTerm)
+}
 
 const addMovieHandler = () => {
   const title = document.getElementById('title').value
@@ -39,19 +44,19 @@ const addMovieHandler = () => {
     extraName.trim() === '' ||
     extraValue.trim() === ''
   ) {
-      alert('pelase enter valid values');
-      return;
+    alert('pelase enter valid values')
+    return
   }
   const newMovie = {
-      info:{
-           title,
-           [extraName]: extraValue, 
-    
-        },
-        id: Math.random(),
-  };
-  movies.push(newMovie);
-  rendermovies();
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  }
+  movies.push(newMovie)
+  rendermovies()
 }
 
-addMovieBtn.addEventListener('click', addMovieHandler);
+addMovieBtn.addEventListener('click', addMovieHandler)
+searchBtn.addEventListener('click', searchMoviesHandler)
