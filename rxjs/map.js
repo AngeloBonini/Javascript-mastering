b1aad8684e5ac6c15157bb5168b4eb0be199f260
Angelo Bonini <abonini2000@gmail.com>
2021-08-06 23:36:42 -0300

    _map_ operator part 1

---
+++ rxjs/map.js
@@ -0,0 +1,7 @@
+import { of } from 'rxjs';
+import { map } from ' rxjs/operators';
+// import { pipe } from 'rxjs/operators';
+
+of(1,2,3,4,5,676,7,67,68,567,534,5).pipe(
+    map(value => value *10)
+).subscribe(console.log);
