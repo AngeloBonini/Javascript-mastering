ce059b077e65f0bcb24eb5e2c2a169650596d5c3
Angelo Bonini <abonini2000@gmail.com>
2021-08-16 20:45:30 -0300

    filter operator, part 3

--- rxjs/filter.js
+++ rxjs/filter.js
@@ -1,7 +1,20 @@
 import { of, fromEvent } from ' rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 
-of(1,2,3,4,4,33,43,4).pipe(
-    filter(value => value > 2)
-).subscribe(console.log);
+// of(1,2,3,4,4,33,43,4).pipe(
+//     filter(value => value > 2)
+// ).subscribe(console.log);
+
+const keyup$ = fromEvent(document, 'keyup');
+const keyCode$ = keyup$.pipe(
+    map(event => event.code)
+);
+
+const enter$ = keyup$.pipe(
+    filter(code => code === 'Enter')
+);
+
+
+enter$.subscribe(console.log);
+keyCode$.subscribe(console.log);
