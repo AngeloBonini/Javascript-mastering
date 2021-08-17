d20077398907c2c15a9ffb46cffb92fc265fad18
Angelo Bonini <abonini2000@gmail.com>
2021-08-16 23:29:03 -0300

    it should work correctely

--- rxjs/src/index.ts
+++ rxjs/src/index.ts
@@ -1,5 +1,19 @@
 import { Observable } from 'rxjs';
 import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+function calculateScrollPercent(element: any) {
+  const {
+    scrollTop,
+    scrollHeight,
+    clinetHeight
+  } = element;
+  return (scrollTop / (scrollHeight - clinetHeight)) * 100;
+}
 
 const scroll$ = fromEvent(document, 'scroll');
-scroll$.subscribe(console.log)
+const progress$ = scroll$.pipe(
+  map(({target})=> calculateScrollPercent(target.dispatchEvent) )
+)
+progress$.subscribe(console.log);
