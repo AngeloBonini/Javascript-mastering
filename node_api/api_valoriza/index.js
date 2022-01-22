fbd8895fd0199a2b32cde59ebcf9f7a2b25a31c4
Angelo Bonini <abonini2000@gmail.com>
2022-01-22 17:06:17 -0300

    removing example files

--- node_api/api_valoriza/index.js
+++
@@ -1,4 +0,0 @@
-function sendEmail(name, email) {
-    console.log(`email sent to: ${name}  from:  ${email}`);
-}
-sendEmail("Gelo", "gelogelo@gmail.com");
--- node_api/api_valoriza/index.ts
+++
@@ -1,5 +0,0 @@
-function sendEmail(name: string, email: string) {
-  console.log(`email sent to: ${name}  from:  ${email}`);
-}
-
-sendEmail("Gelo", "gelogelo@gmail.com");
